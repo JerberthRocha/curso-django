fabb7eb0a549540b5a03069bb409641f9638c383
JerberthRocha <jerberthrs@gmail.com>
2022-07-29 17:55:01 -0400

    Removendo contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import home, contato, sobre
+from .views import home
 
 urlpatterns = [
     path('', home), # 127.0.0.1:8000
-    path('contato/', contato), # 127.0.0.1:8000/contato/
-    path('sobre/', sobre), # 127.0.0.1:8000/sobre/
 ]
