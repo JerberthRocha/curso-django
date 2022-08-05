a42112c8966db635129e1f596549d33bf6fda7cf
JerberthRocha <jerberthrs@gmail.com>
2022-08-05 13:02:57 -0400

    Mostrando erro 404 para páginas que não existem

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from django.http import Http404
 from .models import Recipe
 
 
@@ -12,11 +13,16 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id, # category__id acessa o id da FK de Recipe
+        category__id=category_id,  # category__id acessa o id da FK de Recipe
         is_published=True
     ).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not Found')
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes
+        'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category |'
     })
 
 
