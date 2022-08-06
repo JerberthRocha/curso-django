5cb0b5fbd5bc7d3b5650c487628b0cf5d05d290a
JerberthRocha <jerberthrs@gmail.com>
2022-08-05 21:27:13 -0400

    Criando branch development

--- utils/recipes/factory.py
+++ utils/recipes/factory.py
@@ -1,12 +1,16 @@
+# from inspect import signature
 from random import randint
-from string import digits
+
 from faker import Faker
+
 
 def rand_ratio():
     return randint(840, 900), randint(473, 573)
 
+
 fake = Faker('pt_BR')
 # print(signature(fake.random_number))
+
 
 def make_recipe():
     return {
