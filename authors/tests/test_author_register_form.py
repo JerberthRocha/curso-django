2a03682c294751f1b5a4b62433157c7282027810
JerberthRocha <jerberthrs@gmail.com>
2022-08-22 12:05:53 -0400

    Testando help_text com teste unitário

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -3,16 +3,32 @@ from django.test import TestCase
 from authors.forms import RegisterForm
 from parameterized import parameterized
 
+
 class AuthorRegisterFormUnitTest(TestCase):
     @parameterized.expand([
-        ('username','Your username'),
-        ('email','Your e-mail'),
-        ('first_name','Ex.: Michael'),
-        ('last_name','Ex.: Scofield'),
-        ('password','Your password'),
-        ('confirm_password','Repeat your password'),
+        ('username', 'Your username'),
+        ('email', 'Your e-mail'),
+        ('first_name', 'Ex.: Michael'),
+        ('last_name', 'Ex.: Scofield'),
+        ('password', 'Your password'),
+        ('confirm_password', 'Repeat your password'),
     ])
-    def test_first_name_placeholder_is_correct(self, field, placeholder):
+    def test_fields_placeholder(self, field, placeholder):
         form = RegisterForm()
         current_placeholder = form[field].field.widget.attrs['placeholder']
         self.assertEqual(current_placeholder, placeholder)
+
+    @parameterized.expand([
+        ('username', (
+            'Obrigatório. 150 caracteres ou menos. ' 
+            'Letras, números e @/./+/-/_ apenas.')),
+        ('email', 'The e-mail must be valid.'),
+        ('password', (
+            'Password must have at least one uppercase letter,'
+            'one lowercase letter and one number. The length should be'
+            'at least 8 characters.')),
+    ])
+    def test_fields_help_text(self, field, needed):
+        form = RegisterForm()
+        current = form[field].field.help_text
+        self.assertEqual(current, needed)
