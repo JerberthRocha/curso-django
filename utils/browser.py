f6f7bb3c120e4657b2075dac0d63dfd513b8534f
JerberthRocha <jerberthrs@gmail.com>
2022-08-29 23:23:00 -0400

    Criando a factory make_chrome_browser

--- utils/browser.py
+++ utils/browser.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from time import sleep
 from selenium.webdriver.chrome.service import Service
 from pathlib import Path
 
@@ -6,10 +7,21 @@ ROOT_PATH = Path(__file__).parent.parent
 CHROMEDRIVER_NAME = 'chromedriver.exe'
 CHROMEDRIVER_PATH = ROOT_PATH / 'bin' / CHROMEDRIVER_NAME
 
-chrome_options = webdriver.ChromeOptions()
-chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
-browser = webdriver.Chrome(
-    service=chrome_service, chrome_options=chrome_options
-)
+def make_chrome_browser(*options):
+    chrome_options = webdriver.ChromeOptions()
 
-browser.get('http://www.google.com.br/')
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
+    browser = webdriver.Chrome(
+        service=chrome_service, chrome_options=chrome_options
+    )
+    return browser
+
+if __name__ == '__main__':
+    browser = make_chrome_browser('--headless')
+    browser.get('http://www.google.com.br/')
+    sleep(3)
+    browser.quit()
