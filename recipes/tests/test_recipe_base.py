6e08344ad79f7194db216d563ccb9b8015918558
JerberthRocha <jerberthrs@gmail.com>
2022-08-31 12:14:20 -0400

    Usando mixins para compartilhar métodos entre classes

--- recipes/tests/test_recipe_base.py
+++ recipes/tests/test_recipe_base.py
@@ -3,10 +3,7 @@ from recipes.models import Recipe, Category
 from django.contrib.auth.models import User
 
 
-class RecipeTestBase(TestCase):
-    def setUp(self) -> None:
-        return super().setUp()
-
+class RecipeMixin:
     def make_category(self, name='Category'):
         return Category.objects.create(name=name)
 
@@ -61,3 +58,8 @@ class RecipeTestBase(TestCase):
             preparation_steps_is_html=preparation_steps_is_html,
             is_published=is_published,
         )
+
+
+class RecipeTestBase(TestCase, RecipeMixin):
+    def setUp(self) -> None:
+        return super().setUp()
