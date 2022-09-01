b05a9027b5e60b9004b3cc3b3c7fc4fb219d7014
JerberthRocha <jerberthrs@gmail.com>
2022-09-01 18:57:06 -0400

    Testando se login_create aceita apenas POST

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -35,5 +35,13 @@ class AuthorsLoginTest(AuthorBaseTest):
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
 
-        # End test
-        self.sleep(2)
+    
+    def test_login_create_raises_404_if_not_POST_method(self):
+        self.browser.get(
+            self.live_server_url + reverse('authors:login_create')
+        )
+
+        self.assertIn(
+            'Not Found',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
