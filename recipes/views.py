a40a6ea60430ca1f97cec8e5cf4f61d036d5a6d4
JerberthRocha <jerberthrs@gmail.com>
2022-09-12 11:42:53 -0400

    Criando Category ListView

--- recipes/views.py
+++ recipes/views.py
@@ -12,28 +12,46 @@ PER_PAGE = int(os.environ.get('PER_PAGE', 3))
 class RecipeListViewBase(ListView):
     model = Recipe
     context_object_name = 'recipes'
-    paginate_by = None
     ordering = ['-id']
     template_name = 'recipes/pages/home.html'
 
-    def get_queryset(self ,*args, **kwargs):
+    def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(
             is_published=True,
         )
         return qs
-    
+
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
         page_obj, pagination_range = make_pagination(
-            self.request, 
-            ctx.get('recipes'), 
+            self.request,
+            ctx.get('recipes'),
             PER_PAGE
         )
         ctx.update(
-            {'recipes':page_obj, 'pagination_range': pagination_range}
+            {'recipes': page_obj, 'pagination_range': pagination_range}
         )
         return ctx
+
+
+class RecipeListViewHome(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+
+class RecipeListViewCategory(RecipeListViewBase):
+    template_name = 'recipes/pages/category.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            category__id=self.kwargs.get('category_id')
+        )
+
+        return qs
+
+class RecipeListViewSearch(RecipeListViewBase):
+    template_name = 'recipes/pages/search.html'
 
 
 def home(request):
