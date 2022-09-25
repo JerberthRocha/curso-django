064af46e822f02b7caa7d1057b6b05060019ba58
JerberthRocha <jerberthrs@gmail.com>
2022-09-25 17:48:25 -0400

    Utilizando gettext para traduções nas views

--- recipes/views.py
+++ recipes/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import render
 from django.db.models.aggregates import Count
 from tag.models import Tag 
 from django.utils import translation
+from django.utils.translation import gettext as _
 import os
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 3))
@@ -87,9 +88,11 @@ class RecipeListViewCategory(RecipeListViewBase):
 
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
+        category_translation = _('Category')
 
         ctx.update({
-            'title': f'{ctx.get("recipes")[0].category.name} - Category |'
+            'title': f'{ctx.get("recipes")[0].category.name} - ' 
+            f'{category_translation} |'
         })
         return ctx
 
