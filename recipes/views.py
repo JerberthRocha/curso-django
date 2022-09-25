70dedcfa2ecba8c2485c78d2f36abe95b692e97a
JerberthRocha <jerberthrs@gmail.com>
2022-09-25 17:37:25 -0400

    Adicionando o idioma no lang do HTML com translation.get_language

--- recipes/views.py
+++ recipes/views.py
@@ -8,6 +8,7 @@ from django.forms.models import model_to_dict
 from django.shortcuts import render
 from django.db.models.aggregates import Count
 from tag.models import Tag 
+from django.utils import translation
 import os
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 3))
@@ -51,8 +52,15 @@ class RecipeListViewBase(ListView):
             ctx.get('recipes'),
             PER_PAGE
         )
+
+        html_language = translation.get_language()
+
         ctx.update(
-            {'recipes': page_obj, 'pagination_range': pagination_range}
+            {
+                'recipes': page_obj, 
+                'pagination_range': pagination_range,
+                'html_language': html_language,
+            }
         )
         return ctx
 
